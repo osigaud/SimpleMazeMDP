aebcc24d80833c1ddf639803feafca1c943ed591
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-04 09:04:45 +0200

    first commit

---
+++ maze_plotter.py
@@ -0,0 +1,259 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+from matplotlib.table import Table
+from toolbox import N, S, E, W
+
+# ------------------- plot functions for a maze like environment ----------------#
+
+
+def coords(width, height, i, j):
+    # processes the starting position of the arrows
+    # i is the width
+    # j is the height
+    x = 0.15 - 0.01 * width + i / width
+    y = 0.8 + 0.02 * height - j / height
+    return x, y
+
+
+def arrow_params(width, height, i, j, action):
+    # processes the starting position of the arrows
+    x, y = coords(width, height, i, j)
+
+    if action == N:
+        return [x, y + 0.08 / height, 0.0, 0.16 / height]
+    elif action == S:
+        return [x, y - 0.08 / height, 0.0, -0.16 / height]
+    elif action == E:
+        return [x + 0.12 / width, y, 0.08 / width, 0.0]
+    elif action == W:
+        return [x - 0.12 / width, y, -0.08 / width, 0.0]
+    else:
+        return [x, y, 0.0, 0.0]
+
+
+def qvalue_params(height, width, i, j, action):
+    x, y = coords(height, width, i, j)
+
+    if action == N:
+        return [x - 0.04 / width, y + 0.32 / height]
+    elif action == S:
+        return [x - 0.04 / width, y - 0.4 / height]
+    elif action == E:
+        return [x + 0.32 / width, y]
+    elif action == W:
+        return [x - 0.24 / width, y]
+    else:
+        return [x, y]
+
+
+def qarrow_params(width, height, i, j, action):
+    # processes the starting position of the arrows
+    x, y = coords(width, height, i, j)
+
+    if action == N:
+        return [x, y + 0.12 / height, 0.0, 0.08 / height]
+    elif action == S:
+        return [x, y - 0.12 / height, 0.0, -0.08 / height]
+    elif action == E:
+        return [x + 0.12 / width, y, 0.02 / width, 0.0]
+    elif action == W:
+        return [x - 0.12 / width, y, -0.02 / width, 0.0]
+    else:
+        return [x, y, 0.0, 0.0]
+
+# -------------------------------------------------------------------------------#
+# maze_mdp plot, used to plot the agent in its environment while processing the V/Q function and policy
+# it can also create videos given a list of V/Q values and a list of policies
+
+
+class MazePlotter:
+    def __init__(self, maze):  # maze defined in the mdp notebook
+        self.maze_attr = maze
+        self.terminal_states = maze.terminal_states
+        plt.ion()
+        self.figW = self.maze_attr.width
+        self.figH = self.maze_attr.height
+        self.figure_history = []
+        self.axes_history = []
+        self.table_history = []
+        self.agent_patch_history = []
+        
+    def init_table(self):  # the states of the mdp are drawn in a matplotlib table, this function creates this table
+        
+        width = 0.1
+        height = 0.2
+        
+        for i in range(self.maze_attr.width):
+            for j in range(self.maze_attr.height):
+                color = np.zeros(3)
+                if self.maze_attr.cells[i][j] == -1:
+                    color[0] = color[1] = color[2] = 0
+                else:
+                    color[0] = color[1] = color[2] = 1
+                self.table_history[-1].add_cell(j, i, width, height, facecolor=color, text='', loc='center')
+        
+        self.axes_history[-1].add_table(self.table_history[-1])
+    
+    def new_render(self):  # initializes the plot by creating its basic components (figure, axis, agent patch and table)
+        # a trace of these components is stored so that the old outputs will last on the notebook
+        # when a new rendering is performed
+        self.figure_history.append(plt.figure(figsize=(self.figW, self.figH)))
+        self.axes_history.append(self.figure_history[-1].add_subplot(111))
+        self.table_history.append(Table(self.axes_history[-1], bbox=[0, 0, 1, 1]))
+        self.agent_patch_history.append(mpatches.Ellipse((-1, -1), 0.06, 0.085, ec="none", fc="dodgerblue", alpha=0.6))
+        self.axes_history[-1].add_patch(self.agent_patch_history[-1])
+        self.init_table()
+    
+    def render(self, agent_state=-1, v=[], policy=[], stochastic=False):  # updates the values of the table
+        # and the agent position and current policy 
+        # some of these components may not show depending on the parameters given when calling this function
+        if len(self.figure_history) == 0:  # new plot
+            self.new_render()
+        
+        self.axes_history[-1].clear()
+        self.axes_history[-1].add_table(self.table_history[-1])
+        
+        # Table values and policy update
+        for i in range(self.maze_attr.width):
+            for j in range(self.maze_attr.height):
+                state = self.maze_attr.cells[i][j]
+                if len(v) > 0:  # working with state values
+                    if len(v.shape) == 1:
+                        self.cell_render_v(v, i, j, state)
+                    else:  # working with state-action values
+                        self.cell_render_q(v, i, j, state)
+                if len(policy) > 0:
+                    if stochastic:
+                        self.render_stochastic_policy(v, policy, i, j, state)
+                    else:
+                        self.render_policy(policy, i, j, state)
+        
+        if agent_state >= 0:
+            x, y = coords(self.maze_attr.width, self.maze_attr.height,
+                          self.maze_attr.state_width[agent_state],
+                          self.maze_attr.state_height[agent_state])
+            self.agent_patch_history[-1].center = x, y
+            self.axes_history[-1].add_patch(self.agent_patch_history[-1])
+        
+        plt.subplots_adjust(left=0.2, bottom=0.2)
+        plt.xticks([])
+        plt.yticks([])
+        self.figure_history[-1].canvas.draw()
+        self.figure_history[-1].canvas.flush_events()
+
+    def cell_render_v(self, v, i, j, state):
+        color = np.zeros(3)
+        if state == -1:
+            color[0] = color[1] = color[2] = 0
+        else:
+            color[0] = color[1] = color[2] = np.min([1 - v[state] / (np.max(v) + 1), 1])
+
+        self.table_history[-1]._cells[(j, i)].set_facecolor(color)
+        self.table_history[-1]._cells[(j, i)]._text.set_text(np.round(v[state], 2))
+
+    def cell_render_q(self, q, i, j, state):
+        color = np.zeros(3)
+        if state == -1:
+            color[0] = color[1] = color[2] = 0
+        else:
+            color[0] = color[1] = color[2] = np.min([1 - np.max(q[state]) / (np.max(q) + 1), 1])
+
+        self.table_history[-1]._cells[(j, i)].set_facecolor(color)
+        self.table_history[-1]._cells[(j, i)]._text.set_text(np.round(np.max(q[state]), 2))
+
+        if not (state == -1 or state in self.terminal_states):
+            qmin = np.min(q[state])
+            if qmin < 0:
+                qmin *= -1  # TODO: should deal better with negative Q values
+            pos_q = q[state] + qmin
+            qmax = np.max(pos_q)
+            norm_q = pos_q / (np.sum(pos_q) - (list(pos_q).count(qmax) * qmax) + 0.1)
+
+            for action in range(len(q[state])):
+
+                x0, y0, x, y = qarrow_params(self.maze_attr.width, self.maze_attr.height, i, j, action)
+
+                arw_color = "green"
+                alpha = 0.9
+                qmax = np.max(q[state])
+
+                if not q[state][action] == qmax:
+                    arw_color = "red"
+                    alpha = norm_q[action]
+
+                if x == 0 and y == 0:
+                    circle = mpatches.Circle((x0, y0), 0.08 / self.maze_attr.width, ec=arw_color, fc=arw_color, alpha=alpha)
+                    self.axes_history[-1].add_patch(circle)
+                else:
+                    self.axes_history[-1].arrow(x0, y0, x, y, alpha=alpha,
+                                                head_width=0.12 / self.maze_attr.width,
+                                                head_length=0.12 / self.maze_attr.height,
+                                                fc=arw_color, ec=arw_color)
+
+    def render_pi(self, policy):
+        for i in range(self.maze_attr.width):
+            for j in range(self.maze_attr.height):
+                state = self.maze_attr.cells[i][j]
+                self.render_policy(policy, i, j, state)
+        self.figure_history[-1].canvas.draw()
+        self.figure_history[-1].canvas.flush_events()
+
+    def render_policy(self, policy, i, j, state):
+        if not (state == -1 or state in self.terminal_states):
+            x0, y0, x, y, = arrow_params(self.maze_attr.width, self.maze_attr.height,
+                                         i, j, policy[state])
+            arw_color = "green"
+            alpha = 0.6
+            self.axes_history[-1].arrow(x0, y0, x, y, alpha=alpha,
+                                        head_width=0.12 / self.maze_attr.width,
+                                        head_length=0.12 / self.maze_attr.height,
+                                        fc=arw_color,
+                                        ec=arw_color)
+
+    def render_stochastic_policy(self, q, policy, i, j, state):
+        color = np.zeros(3)
+        if state in self.maze_attr.walls:
+            color[0] = color[1] = color[2] = 0
+        else:
+            color[0] = color[1] = color[2] = np.min([1 - np.max(q[state]) / (np.max(q) + 1), 1])
+
+        self.table_history[-1]._cells[(j, i)].set_facecolor(color)
+        self.table_history[-1]._cells[(j, i)]._text.set_text(np.round(np.max(q[state]), 2))
+
+        if not (state == -1 or state in self.terminal_states):
+            qmin = np.min(q[state])
+            if qmin < 0:
+                qmin *= -1  # TODO: should deal better with negative Q values
+            pos__q = q[state] + qmin
+            qmax = np.max(pos__q)
+            norm_q = pos__q / (np.sum(pos__q) - (list(pos__q).count(qmax) * qmax) + 0.1)
+
+            for action in range(len(q[state])):
+                x0, y0, x, y = qarrow_params(self.maze_attr.width,
+                                             self.maze_attr.height, i, j, action)
+
+                q_x, q_y = qvalue_params(self.maze_attr.width,
+                                         self.maze_attr.height, i, j, action)
+                arw_color = "green"
+                alpha = 0.9
+                qmax = np.max(q[state])
+
+                proba = policy[state][action]
+                print(proba)
+                plt.text(q_x, q_y, "{:.2f}".format(proba))
+
+                if not q[state][action] == qmax:
+                    arw_color = "red"
+                    alpha = norm_q[action]
+
+                if x == 0 and y == 0:
+                    circle = mpatches.Circle((x0, y0), 0.02, ec=arw_color, fc=arw_color, alpha=alpha)
+                    self.axes_history[-1].add_patch(circle)
+                else:
+                    self.axes_history[-1].arrow(x0, y0, x, y, alpha=alpha,
+                                                head_width=0.03, head_length=0.03, fc=arw_color, ec=arw_color)
+
+    def save_fig(self, title):
+        self.figure_history[-1].savefig(title)
---
+++ mdp.py
@@ -0,0 +1,101 @@
+import numpy as np
+from toolbox import discreteProb
+
+
+class SimpleActionSpace:  # class describing the action space of the markov decision process
+    def __init__(self, action_list=[], nactions=0):
+        if len(action_list) == 0:
+            self.actions = np.array([a for a in range(nactions)])
+        else:
+            self.actions = action_list
+            
+        self.size = len(self.actions)
+
+    def sample(self, prob_list=None):
+        # returns an action drawn according to the prob_list distribution,
+        # if the param is not set, then it is drawn from a uniform distribution
+        if prob_list is None:
+            prob_list = np.ones(self.size)/self.size
+            
+        index = discreteProb(prob_list) 
+        return self.actions[index]
+
+    
+class Mdp:  # defines a Markov Decision Process
+
+    def __init__(self, nb_states, action_space, start_distribution, transition_matrix,
+                  reward_matrix, plotter, gamma=0.9, terminal_states=[], timeout=50):
+        assert (timeout > 10), 'timeout too short:' + timeout
+        self.nb_states = nb_states
+        self.terminal_states = terminal_states
+        self.action_space = action_space
+        self.current_state = -1  # current position of the agent in the maze, it is set by the reset() method
+        self.timeout = timeout  # maximum length of an episode
+        self.timestep = 0 
+        self.P0 = start_distribution  # distribution used to draw the first state of the agent, used in method reset()
+        self.P = transition_matrix
+        self.r = reward_matrix
+        self.plotter = plotter  # used to plot the maze
+        self.gamma = gamma  # discount factor
+        self.last_action_achieved = False  # used to tell whether the last state has been reached or not (see done())
+
+    def reset(self, uniform=False):  # initializes an episode and returns the state of the agent
+        # if uniform is set to False, the first state is drawn according to the P0 distribution,
+        # else it is drawn from a uniform distribution over all the states except for walls
+        
+        if uniform:
+            prob = np.ones(self.nb_states)/self.nb_states
+            self.current_state = discreteProb(prob)
+        else:
+            self.current_state = discreteProb(self.P0)
+            
+        self.timestep = 0
+        self.last_action_achieved = False
+        return self.current_state
+
+    def done(self):  # returns True if the episode is over
+        if self.last_action_achieved:
+            return True
+        if self.current_state in self.terminal_states:  # done when a terminal state is reached
+            # the terminal states are actually a set of states from which any action leads to an added imaginary state,
+            # the "well", with a reward of 1. To know if the episode is over, we have to check
+            # whether the agent is on one of these last states and performed the action that gives it its last reward
+            self.last_action_achieved = True
+        return self.timestep == self.timeout  # done when timeout reached
+
+    def step(self, u, deviation=0):  # performs a step forward in the environment,
+        # if you want to add some noise to the reward, give a value to the deviation param 
+        # which represents the mean μ of the normal distribution used to draw the noise 
+        
+        noise = deviation*np.random.randn()  # generate noise, useful for RTDP
+
+        # r is the reward of the transition, you can add some noise to it
+        reward = self.r[self.current_state, u] + noise
+        
+        # the state reached when performing action u from state x is sampled 
+        # according to the discrete distribution self.P[x,u,:]
+        state = discreteProb(self.P[self.current_state, u, :])
+
+        self.timestep += 1 
+
+        info = {"State transition probabilities": self.P[self.current_state, u, :],
+                "reward's noise value": noise}  # can be used when debugging
+
+        self.current_state = state
+        done = self.done()  # checks if the episode is over
+        
+        return [state, reward, done, info]
+    
+    def new_render(self):  # initializes a new environment rendering (a plot defined by a figure, an axis...)
+        self.plotter.new_render()
+    
+    def render(self, v=[], policy=[], agent_pos=-1):  # outputs the agent in the environment with values V (or Q)
+        if agent_pos > -1:
+            self.plotter.render(agent_state=agent_pos, v=v)
+        elif self.current_state > -1:  # and not self.last_action_achieved:
+            self.plotter.render(agent_state=self.current_state, v=v, policy=policy)
+        else:
+            self.plotter.render(v=v)
+        
+    def save_fig(self, title):  # saves the current output into the disk
+        self.plotter.save_fig(title)
