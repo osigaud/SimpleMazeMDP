53127282ae9f90a52215ec2d877cc8b218485289
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-04 09:20:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='mazemdp',
+    url='https://github.com/osigaud/SimpleMazeMDP',
+    author='Olivier Sigaud',
+    author_email='Olivier.Sigaud@upmc.fr',
+    # Needed to actually package something
+    packages=['mazemdp'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An simple maze to test dynamic programming and tabular reinforcement learning algorithms',
+    long_description=open('README.md').read(),
+)
