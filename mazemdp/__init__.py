50a6c0d131c19b4a55d7aa06ded83558970f8d0e
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-04 11:12:52 +0200

    removed create_random_maze from __init__.py

--- mazemdp/__init__.py
+++ mazemdp/__init__.py
@@ -2,5 +2,5 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mazemdp.maze import build_maze, create_random_maze
+from mazemdp.maze import build_maze
 from mazemdp.toolbox import random_policy, softmax, egreedy_loc, egreedy
