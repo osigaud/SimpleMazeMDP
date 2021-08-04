e74850165c072c1a924f08e978d08c6e27ff1081
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-04 10:18:44 +0200

    added files

---
+++ mazemdp/__init__.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+from maze import build_maze
+from maze import create_random_maze
+from toolbox import random_policy, softmax, egreedy_loc, egreedy
