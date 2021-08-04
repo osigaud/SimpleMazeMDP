ba17063baa97ce94ab9ac6a2d74e67b74967e138
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-04 10:35:34 +0200

    improved init.py

--- mazemdp/__init__.py
+++ mazemdp/__init__.py
@@ -1,8 +1,6 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-import maze
-import toolbox
 
-from maze import build_maze, create_random_maze
-from toolbox import random_policy, softmax, egreedy_loc, egreedy
+from mazemdp.maze import build_maze, create_random_maze
+from mazemdp.toolbox import random_policy, softmax, egreedy_loc, egreedy
