628c339507807a70a82eb7d601559700294810f3
Antonin Raffin <antonin.raffin@ensta.org>
2021-08-11 17:42:21 +0200

    Add hash to version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import os
 from setuptools import setup
+import subprocess
 
 with open(os.path.join("mazemdp", "version.txt"), "r") as file_handler:
     __version__ = file_handler.read().strip()
+
+# Taken from PyTorch code to have a different version per commit
+hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=".").decode('ascii').strip()
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
@@ -15,7 +19,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'plotly>=5.1.0'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version=f'0.1@{hash}',
     # The license can be anything you like
     license='MIT',
     description='An simple maze to test dynamic programming and tabular reinforcement learning algorithms',
