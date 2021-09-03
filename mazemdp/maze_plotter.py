deca3f4179f4de24cd4e9befa67557828e11d677
osigaud <Olivier.Sigaud@upmc.fr>
2021-09-03 09:14:46 +0200

    adjusting the position of arrows in colabs

--- mazemdp/maze_plotter.py
+++ mazemdp/maze_plotter.py
@@ -69,9 +69,9 @@ def coords(width, height, i, j):
     # i is the width
     # j is the height
     # x = 0.12 - 0.04 / width + i / width
-    # y = 0.87 + 0.04 / height - j / height
+    y = 0.89 + 0.04 / height - j / height
     x = (0.44 + i) / width
-    y = (3.52 - j) / height
+    # y = (3.52 - j) / height
     return x, y
 
 
