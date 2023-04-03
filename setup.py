6baab62dbc6799afc8873b56be839b8006f3d5b2
Benjamin Piwowarski <benjamin@piwowarski.fr>
2023-04-03 10:28:39 +0200

    Cleaned up the repository layout and setup files

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-import os
-import subprocess
-
 from setuptools import setup
 
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name="mazemdp",
-    url="https://github.com/osigaud/SimpleMazeMDP",
-    author="Olivier Sigaud",
-    author_email="Olivier.Sigaud@isir.upmc.fr",
-    # Needed to actually package something
-    packages=["mazemdp"],
-    # Needed for dependencies
-    install_requires=["numpy", "matplotlib"],
-    # *strongly* suggested for sharing
-    # version=f"{__version__}.dev0+{hash}",
-    # The license can be anything you like
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    license="MIT",
-    description="An simple maze to test dynamic programming and tabular reinforcement learning algorithms",
-    long_description=open("README.md").read(),
-)
+setup()
