small fix in softmax

src/mazemdp/toolbox.py
@@ -41,9 +41,10 @@ def softmax(q, x, temperature):
     # Output :
     # - p : probability of each action according to the soft-max distribution
 
-    p = np.zeros((len(q[x])))
+    nb_options = len(q[x])
+    p = np.zeros(nb_options)
     sump = 0
-    for i in range(len(p)):
+    for i in range(nb_options):
         p[i] = np.exp((q[x, i] / temperature).round(5))
         sump += p[i]
 